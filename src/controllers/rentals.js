66fd1d93674f62e627369f33bbbda69655b26ca8
Vitor <vitormuller66@gmail.com>
2022-03-07 11:07:56 -0300

    feat: get rentals ok

--- src/controllers/rentals.js
+++ src/controllers/rentals.js
@@ -35,3 +35,95 @@ export async function postRentals(req, res) {
         res.sendStatus(500);
     }
 }
+
+export async function getRentals(req, res) {
+
+    const { customerId } = req.query;
+    const { gameId } = req.query;
+
+    if (customerId) {
+        let rentals = await connection.query(`
+            SELECT * FROM rentals WHERE "customerId"=$1
+        `, [customerId])
+        const resultGames = await connection.query(`
+            SELECT 
+                games.id, 
+                games."categoryId", 
+                games.name AS "name",
+                categories.name AS "categoryName"
+            FROM games 
+                JOIN categories ON games."categoryId" = categories.id
+        `);
+
+        const resultCustomers = await connection.query(`SELECT id, name FROM customers `);
+
+        rentals = rentals.rows.map(rental => ({
+            ...rental,
+            customer: resultCustomers.rows.find(customer => customer.id === rental.customerId),
+            game: resultGames.rows.find(game => game.id === rental.gameId)
+        }));
+
+        if (rentals.length > 0) {
+
+            res.send(rentals);
+        } else {
+            res.send("Não existe aluguel deste usuario")
+        }
+
+
+    } else if (gameId) {
+        let rentals = await connection.query(`
+            SELECT * FROM rentals WHERE "gameId"=$1
+        `, [gameId])
+        const resultGames = await connection.query(`
+            SELECT 
+                games.id, 
+                games."categoryId", 
+                games.name AS "name",
+                categories.name AS "categoryName"
+            FROM games 
+                JOIN categories ON games."categoryId" = categories.id
+        `);
+
+        const resultCustomers = await connection.query(`SELECT id, name FROM customers`);
+
+        rentals = rentals.rows.map(rental => ({
+            ...rental,
+            customer: resultCustomers.rows.find(customer => customer.id === rental.customerId),
+            game: resultGames.rows.find(game => game.id === rental.gameId)
+        }));
+
+        if (rentals.length > 0) {
+
+            res.send(rentals);
+        } else {
+            res.send("Não existem registros!")
+        }
+
+    } else {
+        let rentals = await connection.query(`
+            SELECT * FROM rentals 
+        `)
+        const resultGames = await connection.query(`
+            SELECT 
+                games.id, 
+                games."categoryId", 
+                games.name AS "name",
+                categories.name AS "categoryName"
+            FROM games 
+                JOIN categories ON games."categoryId" = categories.id
+        `);
+
+        const resultCustomers = await connection.query(`SELECT id, name FROM customers`);
+
+        rentals = rentals.rows.map(rental => ({
+            ...rental,
+            customer: resultCustomers.rows.find(customer => customer.id === rental.customerId),
+            game: resultGames.rows.find(game => game.id === rental.gameId)
+        }));
+
+        res.send(rentals);
+    }
+
+
+}
--- src/routes/rentalsRouter.js
+++ src/routes/rentalsRouter.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { postRentals } from "../controllers/rentals.js";
+import { getRentals, postRentals } from "../controllers/rentals.js";
 import validateRental from "../middlewares/validateRentals.js";
 
 const rentalsRouter = Router();
 
 rentalsRouter.post("/rentals", validateRental, postRentals)
-
+rentalsRouter.get("/rentals", getRentals)
 export default rentalsRouter
